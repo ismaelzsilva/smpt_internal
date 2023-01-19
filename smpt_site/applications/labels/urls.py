d501c2a6eec9d1a840f7f79597db5571982c58b6
ismaelzsilva <ismaelrzsilva.accounts@protonmail.com>
2023-01-19 11:13:00 +0100

    remove comments

--- smpt_site/applications/labels/urls.py
+++ smpt_site/applications/labels/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('create_of', views.CreateOF.as_view(), name='create_of'),
     path('delete_of/<pk>', views.DeleteOF.as_view(), name='delete_of'),
     path('create_labels', views.CreateLabels.as_view(), name='create_labels'),
-#     path('labels_pdf', views.LabelsPDF.as_view(), name='labels_pdf'),
 ]
